de1be80b2b9723258c1672e0a8b547c2bb6101cc
Dino Drokan <dino.ddrokan@gmail.com>
2019-12-02 21:21:53 +0100

    Implementirano dodavanje proizvoda u bazu Iteracija kroz pocetnu tablicu podataka i dodavanje redaka u relaciju proizvod. Dodaju se samo oni retci koji imaju radni nalog(RN. stupac nije prazan)

--- Izrada radnih naloga/Ucitaj iz excela u bazu.py
+++ Izrada radnih naloga/Ucitaj iz excela u bazu.py
@@ -2,6 +2,7 @@ import pandas as pd
 from mysql.connector import MySQLConnection, Error
 import datetime as dt
 
+
 #vezaSaBazom je MySQLConnection objekt
 vezaSaBazom = MySQLConnection(host='localhost',
                                database='emd',
@@ -27,7 +28,26 @@ def unesiNarudzbu(brojNarudzbe, rok, nacrt):
 #Funkcija je analogna funkciji unesiNarudzbu
 def unesiProizvod(nacrt, nazivArtikla, materijal, dimenzija, duljina, cnc1, cnc2):
     upit = 'INSERT INTO proizvod(nacrt, nazivArtikla, materijal, dimenzija, duljina, cnc1, cnc2)' + '\nVALUES(%s,%s,%s,%s,%s,%s,%s)'
+    
+    
+    ##Ciscenje podataka
+    #Dimenzija mora sadrzavati samo brojke, rezanje stringa nakon pojavljivanja slova 
+    if dimenzija[0] == 'Ø':
+        dimenzija = dimenzija[1:]
+    if dimenzija[0] == 'S':
+       dimenzija = dimenzija[2:]
+    
     podaci = (nacrt, nazivArtikla, materijal, dimenzija, duljina, cnc1, cnc2)
+    
+    #Stvaranje liste iz tuple-a kako bi se mogli modificirati podaci
+    #Iteracija kroz podatke u potrazi za nan vrijednostima
+    #Nan vrijednost se mijenja sa praznim stringom
+    lista = list(podaci)
+    for (i, item) in enumerate(lista):
+        if pd.isna(item):
+            lista[i] = ""
+            
+    podaci = tuple(lista)
     
     try:
         cursor.execute(upit, podaci)
@@ -35,6 +55,7 @@ def unesiProizvod(nacrt, nazivArtikla, materijal, dimenzija, duljina, cnc1, cnc2
         
     except Error as error:
         print(error)
+        print("\n")
 
 #Funkcija je analogna funkciji unesiNarudzbu    
 def unesiRadniNalog(brojNaloga, brojNarudzbe, nacrt):
@@ -64,6 +85,18 @@ def unesiKolicinuProizvoda(brojNarudzbe, nacrt, kom):
 #Header oznacava pocetak redaka tablice
 tablicaNaloga = pd.read_excel('pregled radnih naloga 2019-10.xls', sheetname='List1', header=1)
 
+#Iteracija kroz cijelu tablicu(dataframe)
+#iterrows() vraca tuple(index, series)
+#series je tip podataka slican arrayu
+for i in tablicaNaloga.iterrows():
+    #Spremi series u redak
+    redak = i[1]
+    #Za svaki redak kojemu stupac RN. nije nan, unesi ga u bazu
+    if pd.isna(redak[0]) == False:
+        unesiProizvod(redak['NACRT'], redak['NAZIV ARTIKLA'], redak['MATERIJAL'], redak['DIMENZIJA'], 
+                      redak['DULJINA'], redak['CNC 1'], redak['CNC 2'])
+    
+    
 
 #Brisanje cursor objekta    
 cursor.close()
