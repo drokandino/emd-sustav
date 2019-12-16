4835abd945a0bcb8552ee64db767c9c0d6dd962f
Dino Drokan <dino.ddrokan@gmail.com>
2019-12-16 20:27:06 +0100

    Zapisivanje podataka u excel template

--- Izrada radnih naloga/Izradi radni nalog.py
+++ Izrada radnih naloga/Izradi radni nalog.py
@@ -1,41 +1,108 @@
 from mysql.connector import MySQLConnection, Error
-
-#Trenutna verzija pise u txt datoteku
-#file postaje objekt pomocu kojeg se manipulira datoteka, "w" - write
-file = open("Radni Nalog.txt", "w")
-
+from openpyxl import load_workbook
 
 try:
     #Povezivanje sa bazom
     vezaSabazom = MySQLConnection(host='localhost',
-                                   database='emd',
+                                   database='emd_novi',
                                    user='root', password='1')
     
     #Cursor objekti za sljanje upita bazi
     cursor = vezaSabazom.cursor(buffered=True)
-    pomocniCursor = vezaSabazom.cursor(buffered=True)
+    cursor2 = vezaSabazom.cursor(buffered=True)
     
-    #Izvrsavanje upita
-    cursor.execute('SELECT * FROM radniNalog;')
+# =============================================================================
+#     #Izvrsavanje upita
+#     cursor.execute('SELECT * FROM radniNalog;')
+#     
+#     #Uzima redak iz izalza prethodng upita. Taj redak pretvara u array
+#     redak = cursor.fetchone()
+# =============================================================================
     
-    #Uzima redak iz izalza prethodng upita. Taj redak pretvara u array
-    redak = cursor.fetchone()
+# =============================================================================
+#     #Zapisivanje u datoteku
+#     file.write("Broj radnog naloga: " + str(redak[0]) + "\n" +
+#                "Broj narudzbe: " + str(redak[1]) + "\n" +
+#                "Nacrt: " + str(redak[2]) + "\n")
+#     
+# =============================================================================
     
-    #Zapisivanje u datoteku
-    file.write("Broj radnog naloga: " + str(redak[0]) + "\n" +
-               "Broj narudzbe: " + str(redak[1]) + "\n" +
-               "Nacrt: " + str(redak[2]) + "\n")
+# =============================================================================
+#     #Dohvacanje podataka za isti radni nalog iz druge tablice
+#     pomocniCursor.execute('SELECT * FROM proizvod WHERE nacrt = "' + str(redak[2]) + '";')
+#     redak2 = pomocniCursor.fetchone()
+#     print(redak2)
+# =============================================================================
     
-    #Dohvacanje podataka za isti radni nalog iz druge tablice
-    pomocniCursor.execute('SELECT * FROM proizvod WHERE nacrt = "' + str(redak[2]) + '";')
-    redak2 = pomocniCursor.fetchone()
-    print(redak2)
-    
-
 
 except Error as error:
     print(error)
 
+
+    
+
+try:
+    #While petlja prolazi kroz svaki nalog
+    cursor.execute('SELECT brojNaloga FROM radniNalog')
+    while True:
+        nalog = cursor.fetchone()
+        if nalog == None:
+            break
+        
+        #Ucitavanje template objekta i ucitavanje sheet-a
+        nalogTemplate = load_workbook('Primjeri naloga/radni nalog template.xlsx') #Work book
+        sheet = nalogTemplate.get_sheet_by_name("List1")    
+        
+        #Zapisivanje broja naloga
+        sheet['J1'] = nalog[0]
+        
+        #Dohvacanje svih nacrta(proizvoda) koji pripadaju trenutnom radnom nalogu
+        cursor2.execute('SELECT nacrt FROM nalogPozicija WHERE nalog = "' + str(nalog[0])+'";')
+        nacrti = cursor2.fetchall()
+        
+        #Za svaki prethodno dohvanceni nacrt, dohvati iz baze naziv proizvoda
+        for i in range(len(nacrti)):
+            cursor2.execute('SELECT naziv FROM pozicija WHERE nacrt= "' + str(nacrti[i][0])+'";')    
+            naziv = cursor2.fetchall()
+            
+            #Zapisivanje u excel(template) nacrta i naziva
+            sheet['C'+str(12+i)] = nacrti[i][0]
+            sheet['B'+str(12+i)] = naziv[0][0]  
+            
+            #Dohvacanje matetrijala iz baze i pisanje u nalog
+            cursor2.execute('SELECT idMaterijal FROM pozicija WHERE nacrt= "' + str(nacrti[i][0])+'";')
+            materijal = cursor2.fetchall()
+            sheet['D'+str(12+i)] = materijal[0][0]
+            sheet['B5'] = materijal[0][0]
+            
+            #Dohvacanje cnc tehnologija i pisanje isith u nalog
+            cursor2.execute('SELECT cnc FROM tehnologijaPozicija WHERE nacrt= "' + str(nacrti[i][0])+'";')
+            cnc = cursor2.fetchall()
+            cnc_lista = []
+            
+            cnc_lista.append(cnc[0][0])
+            
+            if cnc[0][0] == 'nema':
+                cnc_lista[0] = ''
+            if len(cnc) > 1 and cnc[1][0] == 'nema':
+                cnc_lista[1]= ''
+            
+            
+            if len(cnc) > 1:
+                cnc_lista.append(cnc[1][0])
+                sheet['J' + str(12+i)] = cnc_lista[0] +'+' + cnc_lista[1]
+            else:
+                sheet['J' + str(12+i)] = cnc_lista[0]
+                
+        
+            
+        #Spremi kao novi file
+        nalogTemplate.save('Radni nalog ' + str(nalog[0]) + '.xlsx')
+
+            
+except Error as error:
+    print(error)
+    
 
 
 cursor.close()
